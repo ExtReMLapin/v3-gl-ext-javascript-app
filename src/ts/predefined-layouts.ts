10d2092a7044800a6aa532f374dfee0958d90fd1
Pierre FICHEPOIL <pierre.fichepoil@gendarmerie.interieur.gouv.fr>
2024-02-13 09:22:01 +0100

    nouveau layout de base

--- src/ts/predefined-layouts.ts
+++ src/ts/predefined-layouts.ts
@@ -14,23 +14,7 @@ const miniRowConfig: LayoutConfig = {
 			componentType: "Content1",
 			width: 10,
 			componentState: { abc: 'Yhea' },
-		} as ComponentItemConfig, {
-			type: ItemType.row,
-			content: [{
-				type: "component",
-				title: "Title 1st",
-				header: { show: "top" },
-				isClosable: false,
-				componentType: "Content1",
-				width: 10,
-				componentState: { abc: 123 },
-			} as ComponentItemConfig, {
-				type: "component",
-				title: "I'm wide",
-				header: { show: "top", popout: false },
-				componentType: "Content2"
-			} as ComponentItemConfig
-		]}]
+		} as ComponentItemConfig]
 	}
 };
 
